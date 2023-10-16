a665bf0ff1024e44015d3cb27d507d4d1369d55e
John Deniel <johndeniel.d37@gmail.com>
2023-10-16 15:43:01 +0000

    pnpm migration

---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>John Deniel</span>,
+  project: {
+    link: 'https://github.com/johndeniel',
+  },
+  chat: {
+    link: 'https://discord.com',
+  },
+  docsRepositoryBase: 'https://github.com/johndeniel',
+  footer: {
+    text: 'Nextra Docs Template',
+  },
+}
+
+export default config
