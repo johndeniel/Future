58315e9cad4f42bbfef59d9aadb45bd49822c139
John Deniel <johndeniel.d37@gmail.com>
2023-11-13 13:12:43 +0000

    fix:font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,19 @@
 import '@styles/globals.css'
 import 'nextra-theme-docs/style.css'
+import localFont from "next/font/local"
+import { Inter as FontSans } from "next/font/google"
+
+
+const fontHeading = localFont({
+  src: "../fonts/CalSans-SemiBold.woff2",
+  variable: "--font-heading",
+})
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
+
 
 export default function Nextra({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => page)
@@ -7,3 +21,6 @@ export default function Nextra({ Component, pageProps }) {
     <Component {...pageProps} />
   )
 }
+
+export const font1 = { fontHeading };
+export const font2 = { fontSans };
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,19 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 import { SkipNavLink } from 'nextra-theme-docs';
-
+import {font1, font2} from './_app';
+import {cn } from "../lib/utils"
 
 const Document = () => {
   return (
     <Html lang="en" suppressHydrationWarning>
       <Head />
-      <body> 
-   
+      <body 
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          font1.fontHeading.variable,
+          font2.fontSans.variable
+        )}
+      > 
         <SkipNavLink styled />
         <Main />
         <NextScript />
