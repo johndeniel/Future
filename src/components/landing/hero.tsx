646015d7df0469ada818b7f5b7c0818d28f792a4
John Deniel <johndeniel.d37@gmail.com>
2023-11-16 03:25:59 +0000

    firebase init

--- src/components/landing/hero.tsx
+++ src/components/landing/hero.tsx
@@ -5,15 +5,54 @@ import type { FC } from 'react';
 import { cn } from "../../lib/utils";
 import { buttonVariants } from "../ui/button";
 
+import { get, ref } from 'firebase/database'
+import { useState, useEffect } from "react"
+import { database } from '../../../firebase'
+
 interface HeroProps {}
 
 const hero: FC<HeroProps> = () => {
+
+  const [imageUrl, setImageUrl] = useState<string | null>(null);
+
+  useEffect(() => {
+    const itemsRef = ref(database, 'items');
+    
+    get(itemsRef)
+      .then((snapshot) => {
+        if (snapshot.exists()) {
+          const childSnapshot = snapshot.val();
+          const firstChildKey = Object.keys(childSnapshot)[0];
+          const data = childSnapshot[firstChildKey]?.imageUrl;
+
+          if (data) {
+            setImageUrl(data);
+          } else {
+            console.log("No image data available");
+          }
+        } else {
+          console.log("No data");
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
     return (
         <React.Fragment>
         {/* Introduction section */}
+   
         <section className="space-y-6 pb-8 pt-6 md:pb-12 md:pt-10 lg:py-32">
           <div className="container flex max-w-[64rem] flex-col items-center gap-4 text-center">
             {/* Link to the GitHub */}
+            <div>
+      {imageUrl ? (
+        <img src={imageUrl} alt="User Avatar" />
+      ) : (
+        <p>No image data available</p>
+      )}
+    </div>
             <Link
               href="/"
               className="rounded-2xl bg-muted px-4 py-1.5 text-sm font-medium"
@@ -31,7 +70,7 @@ const hero: FC<HeroProps> = () => {
             </p>
             {/* Buttons */}
             <div className="space-x-4">
-              <Link href={"/"} className={cn(buttonVariants({ size: "lg" }))}>
+              <Link href={"/skylark/architecture"} className={cn(buttonVariants({ size: "lg" }))}>
                 Get Started
               </Link>
               <Link
