0acfbe63b79b50f8f032a79f2a7ad2d638c827f1
John Deniel <johndeniel.d37@gmail.com>
2023-11-21 10:34:35 +0000

    dark theme

--- theme.config.tsx
+++ theme.config.tsx
@@ -24,46 +24,8 @@ const config: DocsThemeConfig = {
       }
     }
   },
-  head: function useHead() {
-    return (
-      <>
-        {/* Favicons, meta */}
-        <link
-          href="./src/public/favicon.png"
-          rel="apple-touch-icon"
-          sizes="180x180"
-        />
-        <link
-          href="./src/public/favicon.png"
-          rel="icon"
-          sizes="512x512"
-          type="image/png"
-        />
-        <link
-          href="./src/public/favicon.png"
-          rel="icon"
-          sizes="16x16"
-          type="image/png"
-        />
-        <link href="./src/public/site.webmanifest" rel="manifest" />
-        <link
-          color="#000000"
-          href="./src/public/favicon.png"
-          rel="mask-icon"
-        />
-        <meta content="en" httpEquiv="Content-Language" />
-      </>
-    );
-  },
   footer: {
     component: Footer,
-  },
-  editLink: {
-    text: 'Edit this page on GitHub →'
-  },
-  feedback: {
-    content: 'Question? Give us feedback →',
-    labels: 'feedback'
   },
   sidebar: {
     titleComponent({ title, type }) {
