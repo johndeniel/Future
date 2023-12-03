560dced1aa8dba7f3c748fde2dbd4148e26453cd
John Deniel <johndeniel.d37@gmail.com>
2023-12-03 02:15:40 +0000

    update:json

--- theme.config.tsx
+++ theme.config.tsx
@@ -23,7 +23,21 @@ const config: DocsThemeConfig = {
         titleTemplate: '%s – johndeniel'
       }
     }
-  }, feedback: {
+  }, 
+  head: function useHead() {
+    return (
+      <>
+        <meta name="msapplication-TileColor" content="#fff" />
+        <meta name="theme-color" content="#fff" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta httpEquiv="Content-Language" content="en" />
+    
+        <link rel="icon" href="/favicon.png" type="image/png" />
+      </>
+    )
+  },
+  
+  feedback: {
     content: 'Question? Give us feedback →',
     labels: 'feedback'
   },
